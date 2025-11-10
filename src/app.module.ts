7c7700da3e9b15d4a9095fc94b5cad1a9545233f
Filipe Rother de Souza <filiperother@icloud.com>
2025-11-10 09:18:01 -0300

    change auth paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
-import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
 import { AuthModule } from './auth/auth.module';
-import { AuthenticateController } from './auth/controllers/authenticate.controller';
 import { InformixService } from './informix/informix.service';
+
+import { CreateAccountController } from './auth/controllers/create-account.controller';
+import { AuthenticateController } from './auth/controllers/authenticate.controller';
 import { CreateServiceOrder } from './controllers/create-service-order.controller';
 import { GetStock } from './controllers/get-stock.controller';
 
--- src/auth/controllers/create-account.controller.ts
+++ src/auth/controllers/create-account.controller.ts
@@ -5,10 +5,10 @@ import {
   Post,
   UsePipes,
 } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { hash } from 'bcryptjs';
 import { z } from 'zod';
-import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+import { ZodValidationPipe } from '../../pipes/zod-validation-pipe';
 import { ApiBody, ApiExcludeEndpoint, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { createZodDto } from 'nestjs-zod';
 
