f033754931b5d9bd0639f6ec5bc28ae917a5a57a
Filipe Rother de Souza <filiperother@icloud.com>
2025-12-23 15:13:25 -0300

    add finish module to service order controller

--- src/core/service-order/service-order.module.ts
+++ src/core/service-order/service-order.module.ts
@@ -5,13 +5,15 @@ import { EquipmentService } from "../equipment/equipment.service";
 import { ServiceOrderService } from "./service-order.service";
 import { EquipmentModule } from "../equipment/equipment.module";
 import { ServiceOrderRepository } from "./service-order.repository";
+import { FinishServiceOrder } from "./controllers/finish-service-order.controller";
 
 @Module({
     imports: [
         EquipmentModule
     ],
     controllers: [
-        CreateServiceOrder
+        CreateServiceOrder,
+        FinishServiceOrder
     ],
     providers: [
         InformixService,
